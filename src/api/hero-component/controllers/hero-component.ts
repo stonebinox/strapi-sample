4f63a02d9a44bc769d4bcb3b977eda964849ee52
Anoop Santhanam <anoop.santhanam@gmail.com>
2025-07-10 20:49:00 +0530

    Initial commit

---
+++ src/api/hero-component/controllers/hero-component.ts
@@ -0,0 +1,7 @@
+/**
+ * hero-component controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::hero-component.hero-component');
---
+++ src/api/hero-component/routes/hero-component.ts
@@ -0,0 +1,7 @@
+/**
+ * hero-component router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::hero-component.hero-component');
