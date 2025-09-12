9b560dfcebb409c2668e607e1da5c4ffa5e7a9f6
Anoop Santhanam <anoop.santhanam@gmail.com>
2025-09-12 10:44:25 +0530

    removed ACL

--- config/plugins.ts
+++ config/plugins.ts
@@ -7,7 +7,6 @@ export default ({ env }) => ({
         secretAccessKey: env("AWS_ACCESS_SECRET"),
         region: env("AWS_REGION"),
         params: {
-          ACL: env("AWS_ACL", "public-read"),
           signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
           Bucket: env("AWS_BUCKET"),
         },
