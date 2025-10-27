1b5eca53118a1a23ef4d329f001122d2757e7d3e
Anoop Santhanam <anoop.santhanam@gmail.com>
2025-10-27 13:02:30 +0530

    Updated structure

---
+++ src/api/property-listing/controllers/property-listing.ts
@@ -0,0 +1,7 @@
+/**
+ * property-listing controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::property-listing.property-listing');
---
+++ src/api/property-listing/routes/property-listing.ts
@@ -0,0 +1,7 @@
+/**
+ * property-listing router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::property-listing.property-listing');
